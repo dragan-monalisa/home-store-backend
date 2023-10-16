700014fc0789ca7d5e05b03f65435cd58d82adc9
Monalisa29 <mihaelamonalisa@yahoo.com>
2023-10-16 16:43:04 +0300

    Remove Sale functionality

--- src/main/java/com/homestore/estate/sale/SaleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.homestore.estate.sale;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleRepository extends JpaRepository<Sale, Long> {
-}
