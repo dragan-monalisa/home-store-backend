700dde100fcd415c85e2d15866379ad35ef03a90
Monalisa29 <mihaelamonalisa@yahoo.com>
2023-10-30 17:23:30 +0200

    Update Property

--- src/main/java/com/homestore/property/PropertyService.java
+++ src/main/java/com/homestore/property/PropertyService.java
@@ -3,7 +3,6 @@ package com.homestore.property;
 import com.homestore.security.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 import java.util.Optional;
 
 public interface PropertyService {
