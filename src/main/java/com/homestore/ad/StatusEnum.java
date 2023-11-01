eac77f7ac71f97519357d60f5bab49218fe89411
Monalisa29 <mihaelamonalisa@yahoo.com>
2023-11-01 17:01:53 +0200

    Refactor Ad

--- src/main/java/com/homestore/ad/StatusEnum.java
+++ src/main/java/com/homestore/ad/StatusEnum.java
@@ -1,8 +1,8 @@
 package com.homestore.ad;
 
 public enum StatusEnum {
-    PENDING,
-    ACTIVE,
-    INACTIVE,
-    REJECTED
+    PENDING,     // The object is waiting for a response from the realtor
+    ACTIVE,      // The object is active/ has been accepted by the realtor
+    INACTIVE,    // The object is inactive
+    REJECTED     // The object has been rejected by the realtor
 }
